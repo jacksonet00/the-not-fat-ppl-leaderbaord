2ce6b4bb17a5303d6137ab37d91938ccbb6b53cb
Jackson Taylor <jacksoneliastaylor@gmail.com>
2022-11-15 12:42:36 -0500

    udpate data

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { Box, Button, ChakraProvider, Heading, Menu, MenuButton, Progress, Spinn
 import { useEffect, useState } from "react";
 import { SunIcon, MoonIcon } from '@chakra-ui/icons';
 
-const currentDay = 27;
+const currentDay = 30;
 
 async function fetchData() {
   const res = await fetch('https://nfp-leaderboard.vercel.app/api/data', {
@@ -43,7 +43,7 @@ const defaultData: FatPerson[] = [
       0, 1, 2, 3, 4, 5, 6, 7,
       8, 9, 10, 11, 12, 13, 14, 15,
       16, 17, 18, 19, 20, 21, 22, 23,
-      24, 25, 26, 27
+      24, 25, 26, 27, 28, 29, 30
     ]
   },
   {
@@ -81,7 +81,7 @@ const defaultData: FatPerson[] = [
     name: 'liza',
     daysCompleted: [
       2, 3, 6, 9,
-      16, 18, 20, 22, 24, 26, 27
+      16, 18, 20, 22, 24, 26, 27, 29
     ]
   },
   { name: 'sarah', daysCompleted: [] },
