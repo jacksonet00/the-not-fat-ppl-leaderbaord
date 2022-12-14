f2a70960bf90429206c3a69cd0fa4be387ddb408
Jackson Taylor <jacksoneliastaylor@gmail.com>
2022-12-13 22:44:46 -0500

    v

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import Link from "next/link";
-import Record from "../components/Record";
 import { Challenge } from "../types/Challenge";
-import { User } from "../types/User";
-
-async function fetchUsers() {
-  const prisma = new PrismaClient();
-  const users = await prisma.users.findMany({
-    include: {
-      completions: true,
-    }
-  });
-  return users;
-}
 
 async function fetchChallenges() {
   const prisma = new PrismaClient();
   return prisma.challenges.findMany();
 }
 
-
-// a string with the class types for a bootstrap button with a grey container
-const buttonClass = "bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center";
 
 function renderChallengeList(challenges: Challenge[]) {
   return challenges.map((challenge) => (
