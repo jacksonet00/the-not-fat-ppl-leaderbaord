3de98615ccf600ad108209f66aa810ed4e9bd0ec
Jackson Taylor <57923799+jacksonet00@users.noreply.github.com>
2023-01-05 13:17:02 -0500

    Update ProfileSmall.tsx

--- components/ProfileSmall.tsx
+++ components/ProfileSmall.tsx
@@ -21,8 +21,8 @@ export default function ProfileSmall({
         if (currentStreakIncludesToday) {
             return (
                 <div className="flex flex-row">
-                    <h1 className={`${currentStreakLength === bestStreakLength ? 'mr-2' : ''}`}>{`🔥 x ${currentStreakLength}`}</h1>
-                    {currentStreakLength === bestStreakLength && <h1>🏆</h1>}
+                    <h1 className={`font-bold ${currentStreakLength === bestStreakLength ? 'mr-2' : ''}`}>{`🔥 x ${currentStreakLength}`}</h1>
+                    {currentStreakLength === bestStreakLength && <h1 className="font-bold">🏆</h1>}
                 </div>
             );
 
@@ -30,8 +30,8 @@ export default function ProfileSmall({
         return (
             <div className="flex flex-row">
                 <Image src="/frozen-fire.svg" height={20} width={20} alt={"streak freeze"} />
-                <h1 className={`${currentStreakLength === bestStreakLength ? 'mr-2' : ''}`}>{`x ${currentStreakLength}`}</h1>
-                {currentStreakLength === bestStreakLength && <h1>🏆</h1>}
+                <h1 className={`font-bold ${currentStreakLength === bestStreakLength ? 'mr-2' : ''}`}>{`x ${currentStreakLength}`}</h1>
+                {currentStreakLength === bestStreakLength && <h1 className="font-bold">🏆</h1>}
             </div>
         );
 
@@ -45,13 +45,13 @@ export default function ProfileSmall({
             </picture> */}
             <div className="flex flex-col">
                 <div className="flex flex-row">
-                    <h1 className="mr-2">{participantName}</h1>
-                    <h1 className="relative bottom-0.5">{`${crown ? '👑' : ''}`}</h1>
+                    <h1 className="font-bold mr-2">{participantName}</h1>
+                    <h1 className="font-bold relative bottom-0.5">{`${crown ? '👑' : ''}`}</h1>
                 </div>
                 <div className="flex flex-row">
-                    {currentStreakLength > 0 ? <h1 className="mr-2">{renderStreakIcon()}</h1> : <></>}
-                    {currentStreakLength !== bestStreakLength && <h1 className="mr-2">🏅x {bestStreakLength}</h1>}
-                    <h1 className="mr-2">✅ x {totalCompletions}</h1>
+                    {currentStreakLength > 0 ? <h1 className="font-bold mr-2">{renderStreakIcon()}</h1> : <></>}
+                    {currentStreakLength !== bestStreakLength && <h1 className="font-bold mr-2">🏅x {bestStreakLength}</h1>}
+                    <h1 className="font-bold mr-2">✅ x {totalCompletions}</h1>
                 </div>
             </div>
 
